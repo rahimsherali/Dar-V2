3c77d8afdbba591cb6a2e6cf651e609c789fa167
rahimsherali <45488469+rahimsherali@users.noreply.github.com>
2021-09-29 21:26:57 +0500

    Add files via upload

---
+++ assets/js/Treeview.js
@@ -0,0 +1,410 @@
+// var data= [{
+//     "id": 107,
+//     "level_name": "How To Use The Knowledge Base",
+//     "level_id": 1,
+//     "parent_id": 0,
+//     "page_slug": "how-to-use-the-knowledge-base",
+//     "page_id": 3,
+//     "text": "How To Use The Knowledge Base",
+//     "href": "\/website\/how-to-use-the-knowledge-base",
+//     "selectedIcon": "",
+//     "selectable": true,
+//     "state": {
+//         "checked": false,
+//         "disabled": false,
+//         "expanded": false,
+//         "selected": false
+//     },
+//     "nodes": [{
+//         "id": 110,
+//         "level_name": "Ask An Expert",
+//         "level_id": 4,
+//         "parent_id": 1,
+//         "page_slug": "ask-an-expert-1",
+//         "page_id": 6,
+//         "text": "Ask An Expert",
+//         "href": "\/website\/ask-an-expert-1",
+//         "selectedIcon": "",
+//         "selectable": true,
+//         "state": {
+//             "checked": false,
+//             "disabled": false,
+//             "expanded": false,
+//             "selected": false
+//         }
+//     }, {
+//         "id": 115,
+//         "level_name": "Meet The Team",
+//         "level_id": 9,
+//         "parent_id": 1,
+//         "page_slug": "meet-the-team",
+//         "page_id": 11,
+//         "text": "Meet The Team",
+//         "href": "\/website\/meet-the-team",
+//         "selectedIcon": "",
+//         "selectable": true,
+//         "state": {
+//             "checked": false,
+//             "disabled": false,
+//             "expanded": false,
+//             "selected": false
+//         }
+//     }]
+// }, {
+//     "id": 109,
+//     "level_name": "Explore The Sustainability Standards",
+//     "level_id": 3,
+//     "parent_id": 0,
+//     "page_slug": "explore-the-sustainability-standards",
+//     "page_id": 5,
+//     "text": "Explore The Sustainability Standards",
+//     "href": "\/website\/explore-the-sustainability-standards",
+//     "selectedIcon": "",
+//     "selectable": true,
+//     "state": {
+//         "checked": false,
+//         "disabled": false,
+//         "expanded": false,
+//         "selected": false
+//     },
+//     "nodes": [{
+//         "id": 111,
+//         "level_name": "Principle 1 Senior Management Commitment",
+//         "level_id": 5,
+//         "parent_id": 3,
+//         "page_slug": "principle-1-senior-management-commitment",
+//         "page_id": 7,
+//         "text": "Principle 1 Senior Management Commitment",
+//         "href": "\/website\/principle-1-senior-management-commitment",
+//         "selectedIcon": "",
+//         "selectable": true,
+//         "state": {
+//             "checked": false,
+//             "disabled": false,
+//             "expanded": false,
+//             "selected": false
+//         }
+//     }, {
+//         "id": 112,
+//         "level_name": "Principle 2 Sourcing And Supply Chain",
+//         "level_id": 6,
+//         "parent_id": 3,
+//         "page_slug": "principle-2-sourcing-and-supply-chain",
+//         "page_id": 8,
+//         "text": "Principle 2 Sourcing And Supply Chain",
+//         "href": "\/website\/principle-2-sourcing-and-supply-chain",
+//         "selectedIcon": "",
+//         "selectable": true,
+//         "state": {
+//             "checked": false,
+//             "disabled": false,
+//             "expanded": false,
+//             "selected": false
+//         }
+//     }, {
+//         "id": 113,
+//         "level_name": "Principle 3 Performance Measures",
+//         "level_id": 7,
+//         "parent_id": 3,
+//         "page_slug": "principle-3-performance-measures",
+//         "page_id": 9,
+//         "text": "Principle 3 Performance Measures",
+//         "href": "\/website\/principle-3-performance-measures",
+//         "selectedIcon": "",
+//         "selectable": true,
+//         "state": {
+//             "checked": false,
+//             "disabled": false,
+//             "expanded": false,
+//             "selected": false
+//         },
+//         "nodes": [{
+//             "id": 116,
+//             "level_name": "Social Accountability",
+//             "level_id": 10,
+//             "parent_id": 7,
+//             "page_slug": "social-accountability",
+//             "page_id": 12,
+//             "text": "Social Accountability",
+//             "href": "\/website\/social-accountability",
+//             "selectedIcon": "",
+//             "selectable": true,
+//             "state": {
+//                 "checked": false,
+//                 "disabled": false,
+//                 "expanded": false,
+//                 "selected": false
+//             }
+//         }, {
+//             "id": 117,
+//             "level_name": "Environmental",
+//             "level_id": 11,
+//             "parent_id": 7,
+//             "page_slug": "environmental",
+//             "page_id": 13,
+//             "text": "Environmental",
+//             "href": "\/website\/environmental",
+//             "selectedIcon": "",
+//             "selectable": true,
+//             "state": {
+//                 "checked": false,
+//                 "disabled": false,
+//                 "expanded": false,
+//                 "selected": false
+//             },
+//             "nodes": [{
+//                 "id": 126,
+//                 "level_name": "Site Management",
+//                 "level_id": 20,
+//                 "parent_id": 11,
+//                 "page_slug": "site-management",
+//                 "page_id": 22,
+//                 "text": "Site Management",
+//                 "href": "\/website\/site-management",
+//                 "selectedIcon": "",
+//                 "selectable": true,
+//                 "state": {
+//                     "checked": false,
+//                     "disabled": false,
+//                     "expanded": false,
+//                     "selected": false
+//                 }
+//             }, {
+//                 "id": 127,
+//                 "level_name": "Resource Management",
+//                 "level_id": 21,
+//                 "parent_id": 11,
+//                 "page_slug": "resource-management",
+//                 "page_id": 23,
+//                 "text": "Resource Management",
+//                 "href": "\/website\/resource-management",
+//                 "selectedIcon": "",
+//                 "selectable": true,
+//                 "state": {
+//                     "checked": false,
+//                     "disabled": false,
+//                     "expanded": true,
+//                     "selected": true
+//                 }
+//             }, {
+//                 "id": 128,
+//                 "level_name": "Inputs",
+//                 "level_id": 22,
+//                 "parent_id": 11,
+//                 "page_slug": "inputs",
+//                 "page_id": 24,
+//                 "text": "Inputs",
+//                 "href": "\/website\/inputs",
+//                 "selectedIcon": "",
+//                 "selectable": true,
+//                 "state": {
+//                     "checked": false,
+//                     "disabled": false,
+//                     "expanded": false,
+//                     "selected": false
+//                 }
+//             }, {
+//                 "id": 134,
+//                 "level_name": "Outputs",
+//                 "level_id": 28,
+//                 "parent_id": 11,
+//                 "page_slug": "outputs",
+//                 "page_id": 30,
+//                 "text": "Outputs",
+//                 "href": "\/website\/outputs",
+//                 "selectedIcon": "",
+//                 "selectable": true,
+//                 "state": {
+//                     "checked": false,
+//                     "disabled": false,
+//                     "expanded": false,
+//                     "selected": false
+//                 }
+//             }]
+//         }, {
+//             "id": 118,
+//             "level_name": "Economic Criteria",
+//             "level_id": 12,
+//             "parent_id": 7,
+//             "page_slug": "economic-criteria",
+//             "page_id": 14,
+//             "text": "Economic Criteria",
+//             "href": "\/website\/economic-criteria",
+//             "selectedIcon": "",
+//             "selectable": true,
+//             "state": {
+//                 "checked": false,
+//                 "disabled": false,
+//                 "expanded": false,
+//                 "selected": false
+//             },
+//             "nodes": [{
+//                 "id": 149,
+//                 "level_name": "Financial Management",
+//                 "level_id": 43,
+//                 "parent_id": 12,
+//                 "page_slug": "financial-management",
+//                 "page_id": 45,
+//                 "text": "Financial Management",
+//                 "href": "\/website\/financial-management",
+//                 "selectedIcon": "",
+//                 "selectable": true,
+//                 "state": {
+//                     "checked": false,
+//                     "disabled": false,
+//                     "expanded": false,
+//                     "selected": false
+//                 }
+//             }, {
+//                 "id": 151,
+//                 "level_name": "Supporting Communities",
+//                 "level_id": 45,
+//                 "parent_id": 12,
+//                 "page_slug": "supporting-communities",
+//                 "page_id": 47,
+//                 "text": "Supporting Communities",
+//                 "href": "\/website\/supporting-communities",
+//                 "selectedIcon": "",
+//                 "selectable": true,
+//                 "state": {
+//                     "checked": false,
+//                     "disabled": false,
+//                     "expanded": false,
+//                     "selected": false
+//                 }
+//             }]
+//         }]
+//     }, {
+//         "id": 114,
+//         "level_name": "Principle 4 Continuous Improvement",
+//         "level_id": 8,
+//         "parent_id": 3,
+//         "page_slug": "principle-4-continuous-improvement",
+//         "page_id": 10,
+//         "text": "Principle 4 Continuous Improvement",
+//         "href": "\/website\/principle-4-continuous-improvement",
+//         "selectedIcon": "",
+//         "selectable": true,
+//         "state": {
+//             "checked": false,
+//             "disabled": false,
+//             "expanded": false,
+//             "selected": false
+//         }
+//     }]
+// }, {
+//     "id": 160,
+//     "level_name": "What Is The Sustainability Programme",
+//     "level_id": 54,
+//     "parent_id": 0,
+//     "page_slug": "what-is-the-sustainability-programme",
+//     "page_id": 56,
+//     "text": "What Is The Sustainability Programme",
+//     "href": "\/website\/what-is-the-sustainability-programme",
+//     "selectedIcon": "",
+//     "selectable": true,
+//     "state": {
+//         "checked": false,
+//         "disabled": false,
+//         "expanded": false,
+//         "selected": false
+//     }
+// }]
+
+
+
+// // $(document).ready(function(){
+
+// //     var toggler = document.getElementsByClassName("caret");
+// //     var i;
+// //     debugger;
+// //     for (i = 0; i < toggler.length; i++) {
+// //       toggler[i].addEventListener("click", function() {
+
+// //         debugger;
+// //         this.parentElement.querySelector(".nested").classList.toggle("active");
+// //         this.classList.toggle("caret-down");
+// //       });
+
+
+// //     }
+
+
+
+// //     toggler[0].parentElement.querySelector(".nested").classList.toggle("active");
+// //     toggler[0].classList.toggle("caret-down");
+
+
+
+// // })
+
+
+
+
+$.fn.extend({
+    treed: function(o) {
+
+        var openedClass = 'fas fa-folder-minus';
+        var closedClass = 'fas fa-folder-plus';
+
+        if (typeof o != 'undefined') {
+            if (typeof o.openedClass != 'undefined') {
+                openedClass = o.openedClass;
+            }
+            if (typeof o.closedClass != 'undefined') {
+                closedClass = o.closedClass;
+            }
+        };
+
+        //initialize each of the top levels
+        var tree = $(this);
+        tree.addClass("tree");
+        tree.find('li').has("ul").each(function() {
+            var branch = $(this); //li with children ul
+            branch.prepend("<i class='fas fa-folder " + closedClass + "'></i>");
+            branch.addClass('branch');
+            branch.on('click', function(e) {
+                if (this == e.target) {
+                    var icon = $(this).children('i:first');
+                    icon.toggleClass(openedClass + " " + closedClass);
+                    $(this).children().children().toggle();
+                }
+
+            })
+
+            branch.children().children().toggle();
+            var icon = $(this).children('i:first');
+
+            icon.toggleClass(openedClass + " " + closedClass);
+            $(this).children().children().toggle();
+        });
+        //fire event from the dynamically added icon
+        tree.find('.branch .indicator').each(function() {
+            $(this).on('click', function() {
+                $(this).closest('li').click();
+            });
+        });
+        //fire event to open branch if the li contains an anchor instead of text
+        tree.find('.branch>a').each(function() {
+            $(this).on('click', function(e) {
+                $(this).closest('li').click();
+                e.preventDefault();
+            });
+        });
+        //fire event to open branch if the li contains a button instead of text
+        tree.find('.branch>button').each(function() {
+            $(this).on('click', function(e) {
+                $(this).closest('li').click();
+                e.preventDefault();
+            });
+        });
+    }
+});
+
+//Initialization of treeviews
+
+$('#tree1').treed();
+
+$('#tree2').treed({ openedClass: 'fas fa-folder-open', closedClass: 'fas fa-folder' });
+
+$('#tree3').treed({ openedClass: 'glyphicon-chevron-right', closedClass: 'glyphicon-chevron-down' });
