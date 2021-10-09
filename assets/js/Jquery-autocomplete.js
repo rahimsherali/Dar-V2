a2809ba8e6ee9424224b8502a8590f21baf4ad9d
TalhaShaikh24 <87939410+TalhaShaikh24@users.noreply.github.com>
2021-10-09 16:35:52 +0500

    Add files via upload

---
+++ assets/js/Jquery-autocomplete.js
@@ -0,0 +1,49 @@
+$(function childName() {
+    var nameTags = [
+        "Azlal",
+        "Abid",
+        "Asif",
+        "Bilal",
+        "Clark",
+        "Erik",
+        "Farhan",
+        "Ghulfam",
+        "Haris",
+        "Jawad",
+        "Javed",
+        "Laik",
+        "Paras",
+        "Pawan",
+        "Rafique",
+        "Sohai",
+        "Shazaib"
+    ];
+    $("#childName").autocomplete({
+        source: nameTags
+    });
+});
+
+$(function motherName() {
+    var motherTags = [
+        "Azlal",
+        "Abid",
+        "Asif",
+        "Bilal",
+        "Clark",
+        "Erik",
+        "Farhan",
+        "Ghulfam",
+        "Haris",
+        "Jawad",
+        "Javed",
+        "Laik",
+        "Paras",
+        "Pawan",
+        "Rafique",
+        "Sohai",
+        "Shazaib"
+    ];
+    $("#motherName").autocomplete({
+        source: motherTags
+    });
+});
